b2e7c998033d72f356a3590f02dec4ade6b20b44
AnkurDas005 <ankurdas286@gmail.com>
2026-01-20 21:53:46 +0530

    use of arrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,47 @@
 #include <raylib.h>
+#include <cmath>
 
 int main() {
     const int screenWidth = 900;
     const int screenHeight = 800;
-    const int radius = 50;
+    const int radius = 5;
+
     InitWindow(screenWidth, screenHeight, "simple");
     SetTargetFPS(60);
 
-    float x0 = screenWidth / 2;
-    float y0 = screenHeight / 2;
+    const int nBalls = 30;
+    float pos_x[nBalls];
+    float pos_y[nBalls];
 
-    float u0 = 2;
-    float v0 = 5;
+    float speed_x[nBalls];
+    float speed_y[nBalls];
 
+    for(int i = 0; i < nBalls; i++) {
+        pos_x[i] = screenWidth / 2.f;
+        pos_y[i] = screenHeight / 2.f;
 
-    float x1 = screenWidth / 2;
-    float y1 = screenHeight / 2;
-
-    float u1 = 4;
-    float v1 = -3;
-
-    float x2 = screenWidth / 2;
-    float y2 = screenHeight / 2;
-
-    float u2 = 7;
-    float v2 = -2;
+        speed_x[i] = (float)((rand() % 200) - 100) / 100.f;
+        speed_y[i] = (float)((rand() % 200) - 100) / 100.f;
+    }
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawCircle(x0, y0, radius, RED);
-        DrawCircle(x1, y1, radius, GREEN);
-        DrawCircle(x2, y2, radius, ORANGE);
+        ClearBackground(BLACK);      
+        for(int i = 0; i < nBalls; i++) {
+            DrawCircle(pos_x[i], pos_y[i], radius, GREEN);
+        }
         EndDrawing();
 
-        x0 = x0 + u0;
-        y0 = y0 + v0;
+        for(int i = 0; i < nBalls; i++) {
+            pos_x[i] = pos_x[i] + speed_x[i];
+            pos_y[i] = pos_y[i] + speed_y[i];
 
-        if(x0 >= screenWidth - radius || x0 <= radius) {
-            u0 = -1 * u0;
-        }
-        if(y0 >= screenHeight - radius || y0 <= radius) {
-            v0 = -1 * v0;
-        }
-
-        x1 = x1 + u1;
-        y1 = y1 + v1;
-
-        if(x1 >= screenWidth - radius || x1 <= radius) {
-            u1 = -1 * u1;
-        }
-        if(y1 >= screenHeight - radius || y1 <= radius) {
-            v1 = -1 * v1;
-        }
-
-        x2 = x2 + u2;
-        y2 = y2 + v2;
-
-        if(x2 >= screenWidth - radius || x2 <= radius) {
-            u2 = -1 * u2;
-        }
-        if(y2 >= screenHeight - radius || y2 <= radius) {
-            v2 = -1 * v2;
+            if(pos_x[i] >= screenWidth - radius || pos_x[i] <= radius) {
+                speed_x[i] = -1 * speed_x[i];
+            }
+            if(pos_y[i] >= screenHeight - radius || pos_y[i] <= radius) {
+                speed_y[i] = -1 * speed_y[i];
+            }
         }
     }
     CloseWindow();
