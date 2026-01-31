b34c3c3c843cc9d521ffe0a885e7c22246c3bde4
AnkurDas005 <ankurdas286@gmail.com>
2026-01-31 09:56:14 +0530

    Added different colors to different balls

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <raylib.h>
 #include <cmath>
 
+Color callscol[3] = {YELLOW, GREEN, ORANGE};
+
 int main() {
     const int screenWidth = 900;
     const int screenHeight = 800;
@@ -9,12 +11,14 @@ int main() {
     InitWindow(screenWidth, screenHeight, "simple");
     SetTargetFPS(60);
 
-    const int nBalls = 30;
+    const int nBalls = 300;
     float pos_x[nBalls];
     float pos_y[nBalls];
 
     float speed_x[nBalls];
     float speed_y[nBalls];
+
+    Color ballclours[nBalls];
 
     for(int i = 0; i < nBalls; i++) {
         pos_x[i] = screenWidth / 2.f;
@@ -22,13 +26,15 @@ int main() {
 
         speed_x[i] = (float)((rand() % 200) - 100) / 100.f;
         speed_y[i] = (float)((rand() % 200) - 100) / 100.f;
+
+        ballclours[i] = callscol[rand() % 3];
     }
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);      
         for(int i = 0; i < nBalls; i++) {
-            DrawCircle(pos_x[i], pos_y[i], radius, GREEN);
+            DrawCircle(pos_x[i], pos_y[i], radius, ballclours[i]);
         }
         EndDrawing();
 
