6a48d10b13d96e51d5615430d7f3e6fa5dcfea31
AnkurDas005 <ankurdas286@gmail.com>
2026-01-31 18:18:22 +0530

    Random Radii

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <raylib.h>
 #include <cmath>
 
-Color callscol[3] = {YELLOW, GREEN, ORANGE};
+Color callscol[3] = {RED, BLUE, ORANGE};
 
 int main() {
     const int screenWidth = 900;
     const int screenHeight = 800;
-    const int radius = 5;
 
     InitWindow(screenWidth, screenHeight, "simple");
     SetTargetFPS(60);
@@ -20,6 +19,8 @@ int main() {
 
     Color ballclours[nBalls];
 
+    float radii[nBalls];
+
     for(int i = 0; i < nBalls; i++) {
         pos_x[i] = screenWidth / 2.f;
         pos_y[i] = screenHeight / 2.f;
@@ -28,13 +29,14 @@ int main() {
         speed_y[i] = (float)((rand() % 200) - 100) / 100.f;
 
         ballclours[i] = callscol[rand() % 3];
+        radii[i] = ((((rand() % 100) / 100.f) * 8) + 2);
     }
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);      
         for(int i = 0; i < nBalls; i++) {
-            DrawCircle(pos_x[i], pos_y[i], radius, ballclours[i]);
+            DrawCircle(pos_x[i], pos_y[i], radii[i], ballclours[i]);
         }
         EndDrawing();
 
@@ -42,10 +44,10 @@ int main() {
             pos_x[i] = pos_x[i] + speed_x[i];
             pos_y[i] = pos_y[i] + speed_y[i];
 
-            if(pos_x[i] >= screenWidth - radius || pos_x[i] <= radius) {
+            if(pos_x[i] >= screenWidth - radii[i] || pos_x[i] <= radii[i]) {
                 speed_x[i] = -1 * speed_x[i];
             }
-            if(pos_y[i] >= screenHeight - radius || pos_y[i] <= radius) {
+            if(pos_y[i] >= screenHeight - radii[i] || pos_y[i] <= radii[i]) {
                 speed_y[i] = -1 * speed_y[i];
             }
         }
