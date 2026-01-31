cc6c6bf456ef99c4547b09d51239faf8d67ad661
AnkurDas005 <ankurdas286@gmail.com>
2026-01-31 19:21:09 +0530

    Now using structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <raylib.h>
 #include <cmath>
 
-Color callscol[3] = {RED, BLUE, ORANGE};
+struct Ball {
+    float pos_x, pos_y;
+    float speed_x, speed_y;
+    float radius;
+    Color color;
+};
 
 int main() {
     const int screenWidth = 900;
@@ -10,45 +15,42 @@ int main() {
     InitWindow(screenWidth, screenHeight, "simple");
     SetTargetFPS(60);
 
-    const int nBalls = 300;
-    float pos_x[nBalls];
-    float pos_y[nBalls];
-
-    float speed_x[nBalls];
-    float speed_y[nBalls];
-
-    Color ballclours[nBalls];
-
-    float radii[nBalls];
+    const int nBalls = 30;
+    Ball balls[nBalls];
 
     for(int i = 0; i < nBalls; i++) {
-        pos_x[i] = screenWidth / 2.f;
-        pos_y[i] = screenHeight / 2.f;
+        balls[i].pos_x = screenWidth / 2.f;
+        balls[i].pos_y = screenHeight / 2.f;
 
-        speed_x[i] = (float)((rand() % 200) - 100) / 100.f;
-        speed_y[i] = (float)((rand() % 200) - 100) / 100.f;
+        balls[i].speed_x = (float)((rand() % 200) - 100) / 100.f;
+        balls[i].speed_y = (float)((rand() % 200) - 100) / 100.f;
 
-        ballclours[i] = callscol[rand() % 3];
-        radii[i] = ((((rand() % 100) / 100.f) * 8) + 2);
+        balls[i].color = {
+            (unsigned char)(rand() % 255),
+            (unsigned char)(rand() % 255),
+            (unsigned char)(rand() % 255),
+            255
+        };
+        balls[i].radius = ((((rand() % 100) / 100.f) * 10) + 10);
     }
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);      
         for(int i = 0; i < nBalls; i++) {
-            DrawCircle(pos_x[i], pos_y[i], radii[i], ballclours[i]);
+            DrawCircle(balls[i].pos_x, balls[i].pos_y, balls[i].radius, balls[i].color);
         }
         EndDrawing();
 
         for(int i = 0; i < nBalls; i++) {
-            pos_x[i] = pos_x[i] + speed_x[i];
-            pos_y[i] = pos_y[i] + speed_y[i];
+            balls[i].pos_x = balls[i].pos_x + balls[i].speed_x;
+            balls[i].pos_y = balls[i].pos_y + balls[i].speed_y;
 
-            if(pos_x[i] >= screenWidth - radii[i] || pos_x[i] <= radii[i]) {
-                speed_x[i] = -1 * speed_x[i];
+            if(balls[i].pos_x >= screenWidth - balls[i].radius || balls[i].pos_x <= balls[i].radius) {
+                balls[i].speed_x = -1 * balls[i].speed_x;
             }
-            if(pos_y[i] >= screenHeight - radii[i] || pos_y[i] <= radii[i]) {
-                speed_y[i] = -1 * speed_y[i];
+            if(balls[i].pos_y >= screenHeight - balls[i].radius || balls[i].pos_y <= balls[i].radius) {
+                balls[i].speed_y = -1 * balls[i].speed_y;
             }
         }
     }
